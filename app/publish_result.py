e3ae36748a6550b2afba9f26a600cfb4a4d8a827
Gorio <gmcbuencamino@gmail.com>
2021-07-17 20:14:36 +0800

    add comments to boilerplate code

--- app/publish_result.py
+++ app/publish_result.py
@@ -3,12 +3,12 @@ from json import dumps
 
 def publish_result(result):
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=os.getenv("RMQ_HOST")))
-    channel = connection.channel()
+        pika.ConnectionParameters(host=os.getenv("RMQ_HOST")))  # connect to rmq
+    channel = connection.channel()  # create a channel (we interact through channels)
 
-    channel.exchange_declare(exchange='test_exchange', exchange_type='fanout')
+    channel.exchange_declare(exchange='test_exchange', exchange_type='fanout')  # create a rabbitmq EXCHANGE to publish to
 
     message = dumps(result)  # convert json back to string because we uased a json input
-    channel.basic_publish(exchange='test_exchange', routing_key='', body=message)
+    channel.basic_publish(exchange='test_exchange', routing_key='', body=message)  # do the publish
     connection.close()
     return
--- listener/listener.py
+++ listener/listener.py
@@ -2,16 +2,16 @@ import pika, os, sys, time
 
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=os.getenv('RMQ_HOST')))
-    channel = connection.channel()
+    channel = connection.channel()  # connect to RMQ server and create a channel
 
-    channel.exchange_declare(exchange='test_exchange', exchange_type='fanout')
-    channel.queue_declare(queue='test_queue')
-    channel.queue_bind(exchange='test_exchange', queue='test_queue')
+    channel.exchange_declare(exchange='test_exchange', exchange_type='fanout')  # create an exchange in case it doesn't exist yet
+    channel.queue_declare(queue='test_queue')  # create the queue that receives messages from the exchange
+    channel.queue_bind(exchange='test_exchange', queue='test_queue')  # tell the queue to listen to exchange
 
-    def callback(ch, method, properties, body):
+    def callback(ch, method, properties, body):  # the actual thing we do. in this case, just print the data
         print(f"[+] Message Received: ${body}")
 
-    channel.basic_consume(queue='test_queue', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='test_queue', on_message_callback=callback, auto_ack=True)  # listen to the queue and do callback if we get a message
 
     print("[+] Waiting for messages. Press CTRL+C to exit.")
     channel.start_consuming()
@@ -19,7 +19,7 @@ def main():
 if __name__ == '__main__':
     try:
         retries = 3
-        while retries:
+        while retries:  # implemented a retry mechanism so that the server will wait until rmq is ready to accept connections
             try:
                 main()
             except pika.exceptions.AMQPConnectionError:
